b3c69e2710581f7fd0b66a7dcee94a8fb25f943d
atroopa <atroopa@gmail.com>
2022-04-13 16:32:30 +0430

    some Functions Created

--- main.jl
+++ main.jl
@@ -2,3 +2,23 @@
 function f(x,y)
   x+y
 end
+
+f(x,y) = x*y
+
+println(f(2,4))
+
+g = f
+
+println(g(155,789))
+
+∑(x,y) = x/y
+
+println(∑(20,5))
+
+
+function gram(x,y)
+  return x*y^3
+  x+y
+end
+
+println(gram(4,2))
